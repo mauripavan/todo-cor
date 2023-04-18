b4bf14dd6816c893bd68d30840c528e3c9551768
mauripavan <mauriciopavan60@gmail.com>
2023-04-18 15:26:31 -0300

    feat: WIP Modal component

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+export interface IModalProps {
+  visible: boolean;
+}
+
+const Modal = () => {
+  return (
+    <div>
+      <p>Hola!</p>
+    </div>
+  );
+};
+
+export default Modal;
