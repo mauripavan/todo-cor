4262fb5d0481b788a3f18b7b2f0b1200896f75bb
mauripavan <mauriciopavan60@gmail.com>
2023-04-20 15:22:12 -0300

    feat: hero icons implemeented

--- src/assets/icons.ts
+++ src/assets/icons.ts
@@ -1,11 +1,9 @@
 /*eslint @typescript-eslint/no-var-requires: "off" */
 
 const logo = require('./icons/logo.png');
-const trash = require('./icons/trash.png');
 const check = require('./icons/check.png');
 
 export const icons = {
   logo,
-  trash,
   check,
 };
